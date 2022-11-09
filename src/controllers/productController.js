8f59036ef55ff1034b0d9886790d768139c30c39
Teo Lunas <rodrigo.lunasp@gmail.com>
2022-11-09 03:44:28 -0300

    se agrega manejo de errores con try catch funcion para traer todos los productos funcions para buscar un producto

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,12 +1,33 @@
-const getAllProducts = (req, res) => {
-    res.json({msg: 'Get all products'})
+const productService = require('../services/productService')
+
+const getAllProducts = async(req, res, next) => {
+    try {
+        const { page, limit } = req.params;
+        const products = await productService.getAllProducts();
+        res.json(products)
+    } catch (error) {
+        next(error)
+    }
 };
 
-const getOneProduct = (req, res) => {
-    res.json({msg: 'Get One Product'})
+const getOneProductById = async(req, res) => {
+    const { id } = req.params;
+    const product = await productService.getOneProductById(id);
+    res.status(200).json(product)
+}
+
+const searchProductByName = async(req, res, next) => {
+    try {
+        const { productName } = req.params;
+        const product = await productService.getOneProductByName(productName);
+        res.status(200).json(product);
+    } catch (error) {
+        next(error)
+    }
 }
 
 module.exports = {
     getAllProducts,
-    getOneProduct
+    getOneProductById,
+    searchProductByName
 };
