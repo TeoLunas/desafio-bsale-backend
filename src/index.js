7b8c9fa21f88d4e1e3504e9cba38bebc63cf21ab
Teo Lunas <rodrigo.lunasp@gmail.com>
2022-11-10 11:51:42 -0300

    uso de middlewares de error

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
+// Importacion de paquetes externos
 const express = require('express');
+
+// Importaciones de Archivos Propios
+const routerApi = require('./routes/index');
+const config = require('./config/config');
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/errorHandler')
+
 const app = express();
 
-const productRouter = require('./routes/productRoutes')
-const routerApi = require('./routes/index')
-
-const port = 3000;
+//Definicion de puerto
+const port = config.port || 3000;
 
 //Middleware
 app.use(express.json());
 
 // Routing
 routerApi(app)
+
+//Middlewars para manejos de error post routing
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 //Inicio del servidor
 app.listen(port, ()=> console.log(`Server en puerto ${port}`))
