e37b4e5fdc8b22e811aa61ebb26fa93715ecd746
Teo Lunas <rodrigo.lunasp@gmail.com>
2022-11-12 14:20:06 -0300

    set variables 3

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,13 @@
-require("dotenv").config();
+require('dotenv').config();
 
-module.exports = {
-    port: process.env.PORT,
-    dbName: process.env.DB_NAME,
-    dbHost: process.env.BD_HOST,
+const config = {
+    env: process.env.NODE_ENV || 'dev',
+    port: process.env.PORT || 3000,
     dbUser: process.env.DB_USER,
-    dbPassword: process.env.DB_PASSWORD
-}
+    dbPassword: process.env.DB_PASSWORD,
+    dbHost: process.env.DB_HOST,
+    dbName: process.env.DB_NAME,
+    dbPort: process.env.DB_PORT,
+};
+
+module.exports = { config };
